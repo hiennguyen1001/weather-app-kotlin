7a52ff6e30d14e693cff5a690e767599ccbc5ad7
Hien Nguyen <hien.nguyen@mingle.com>
2018-04-27 10:52:20 +0700

    [r] Update shell scripts

--- code_quality_tools/run-gradle-android.sh
+++ code_quality_tools/run-gradle-android.sh
@@ -12,6 +12,14 @@ else
     REPORTER=Saddler::Reporter::Github::PullRequestReviewComment
 fi
 
+cat presentation/build/reports/ktlint/ktlint-release.xml \
+    | checkstyle_filter-git diff origin/master \
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
+
+cat domain/build/reports/ktlint/ktlint-release.xml \
+    | checkstyle_filter-git diff origin/master \
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
+
 cat data/build/reports/ktlint/ktlint-release.xml \
     | checkstyle_filter-git diff origin/master \
     | saddler report --require saddler/reporter/github --reporter $REPORTER
--- data/src/main/java/hiennguyen/me/weatherapp/data/models/remote/WeatherData.kt
+++ data/src/main/java/hiennguyen/me/weatherapp/data/models/remote/WeatherData.kt
@@ -4,14 +4,12 @@ import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import io.objectbox.relation.ToMany
 
-
 data class Coord(var lon: Double?, var lat: Double?)
 
 data class City(var id: Int?, var name: String?, var coord: Coord, var country: String?, var population: Int?)
 
 @Entity
-class Temp(@Id var id: Long, var day: Double?, var min: Double?, var max: Double?, var night: Int?, var eve: Double?, var morn:
-Double?)
+class Temp(@Id var id: Long, var day: Double?, var min: Double?, var max: Double?, var night: Int?, var eve: Double?, var morn: Double?)
 
 @Entity
 class Weather(@Id var id: Long?, var main: String?, var description: String?, var icon: String?)
