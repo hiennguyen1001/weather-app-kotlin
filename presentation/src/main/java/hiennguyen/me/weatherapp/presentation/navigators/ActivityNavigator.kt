b52b5667c82a82f73b1c0dcbfe571d238a3247d0
Hien Nguyen <hien.nguyen@mingle.com>
2018-04-27 17:07:06 +0700

    error commit

--- presentation/src/main/java/hiennguyen/me/weatherapp/presentation/navigators/ActivityNavigator.kt
+++ presentation/src/main/java/hiennguyen/me/weatherapp/presentation/navigators/ActivityNavigator.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.*
+import android.os.*
 import android.os.Parcelable
 import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
@@ -12,8 +12,6 @@ import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
 
 import javax.inject.Inject
-
-
 
 
 open class ActivityNavigator @Inject
