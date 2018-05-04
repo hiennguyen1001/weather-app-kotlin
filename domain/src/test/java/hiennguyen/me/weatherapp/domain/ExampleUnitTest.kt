531345d6b72a896987ef3b5f5e967c5674a78a61
Hien Nguyen <hien.nguyen@mingle.com>
2018-05-04 14:42:37 +0700

    add run assembleDebug, update UnitTest file kt

---
+++ domain/src/test/java/hiennguyen/me/weatherapp/domain/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package hiennguyen.me.weatherapp.domain
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun additionIsCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
